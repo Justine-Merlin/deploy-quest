8e480f4060c7168a502e27d16440e99ee396699e
JustineM <justine.merlin94@gmail.com>
2023-03-28 16:44:02 +0200

    modif

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -30,7 +30,7 @@ function App() {
     <div>
       <Header>
         <Container>
-          <h1>Wilders Book with CD !!!</h1>
+          <h1>Wilders Book with CD !!! Youhou Ca marche !</h1>
         </Container>
       </Header>
       <Container>
